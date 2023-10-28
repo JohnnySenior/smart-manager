ca1f598c1ba366879fb92542d692cfcf7e58e216
Zafar <icom@MACKBOOK>
2023-10-28 21:20:10 +0300

    BROKERS: Create Date Time Broker

---
+++ SmartManager/Services/Foundations/Applicants/ApplicantService.cs
@@ -0,0 +1,10 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+namespace SmartManager.Services.Foundations.Applicants
+{
+    public class ApplicantService
+    {
+    }
+}
