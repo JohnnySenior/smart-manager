9b42aa2507bc852c4141ecebd7364d637539abb1
Zafar <icom@MACKBOOK>
2023-10-28 21:56:49 +0300

    FOUNDATIONS: CRUD Group

---
+++ SmartManager/Services/Foundations/Groups/GroupService.cs
@@ -0,0 +1,42 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using SmartManager.Brokers.Storages;
+using SmartManager.Models.Groups;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartManager.Services.Foundations.Groups
+{
+    public class GroupService : IGroupService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public GroupService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<Group> AddGroupAsync(Group group) =>
+            await this.storageBroker.InsertGroupAsync(group);
+
+        public async ValueTask<Group> RetrieveGroupByIdAsync(Guid groupid) =>
+            await this.storageBroker.SelectGroupByIdAsync(groupid);
+
+        public IQueryable RetrieveAllGroups() =>
+            this.storageBroker.SelectAllGroups();
+
+        public async ValueTask<Group> ModifyGroupAsync(Group group) =>
+            await this.storageBroker.UpdateAppolicantAsync(group);
+
+        public async ValueTask<Group> RemoveGroupAsync(Guid groupid)
+        {
+            Group group = await this.storageBroker.SelectGroupByIdAsync(groupid);
+
+            return await this.storageBroker.DeleteGroupAsync(group);
+        }
+    }
+}
---
+++ SmartManager/Services/Foundations/Groups/IGroupService.cs
@@ -0,0 +1,22 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using SmartManager.Models.Applicants;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using SmartManager.Models.Groups;
+
+namespace SmartManager.Services.Foundations.Groups
+{
+    public interface IGroupService
+    {
+        ValueTask<Group> AddGroupAsync(Group group);
+        ValueTask<Group> RetrieveGroupByIdAsync(Guid groupid);
+        IQueryable RetrieveAllGroups();
+        ValueTask<Group> ModifyGroupAsync(Group group);
+        ValueTask<Group> RemoveGroupAsync(Guid groupid);
+    }
+}
