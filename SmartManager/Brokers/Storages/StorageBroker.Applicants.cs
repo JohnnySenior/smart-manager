db7ead2039bbd299848898b0a299efe13d7ff67e
Zafar <icom@MACKBOOK>
2023-10-28 21:16:24 +0300

    BROKERS: CRUD Applicant

--- SmartManager/Brokers/Storages/StorageBroker.Applicants.cs
+++ SmartManager/Brokers/Storages/StorageBroker.Applicants.cs
@@ -2,13 +2,33 @@
 // Copyright (c) Tarteeb LLC
 // Managre quickly and easy
 //===========================
+
 using Microsoft.EntityFrameworkCore;
 using SmartManager.Models.Applicants;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SmartManager.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Applicant> Applicants { get; set; }
+
+        public async ValueTask<Applicant> InsertApplicantAsync(Applicant applicant) =>
+            await InsertAsync(applicant);
+
+        public IQueryable<Applicant> SelectAllApplicants() =>
+            SelectAll<Applicant>().AsQueryable();
+
+        public async ValueTask<Applicant> SelectApplicantByIdAsync(Guid applicantId) =>
+            await SelectAsync<Applicant>(applicantId);
+
+        public async ValueTask<Applicant> UpdateAppolicantAsync(Applicant applicant) =>
+            await UpdateAsync(applicant);
+
+        public async ValueTask<Applicant> DeleteApplicantAsync(Applicant applicant) =>
+            await DeleteAsync(applicant);
     }
 }
+
