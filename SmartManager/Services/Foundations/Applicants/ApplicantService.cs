b29746ab99390ff33fc6119a1018be1db80e505a
Zafar <icom@MACKBOOK>
2023-10-28 21:31:24 +0300

    FOUNDATIONS: CRUD Applicant

--- SmartManager/Services/Foundations/Applicants/ApplicantService.cs
+++ SmartManager/Services/Foundations/Applicants/ApplicantService.cs
@@ -2,9 +2,43 @@
 // Copyright (c) Tarteeb LLC
 // Managre quickly and easy
 //===========================
+
+using SmartManager.Brokers.DateTimes;
+using SmartManager.Brokers.Loggings;
+using SmartManager.Brokers.Storages;
+using SmartManager.Models.Applicants;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
 namespace SmartManager.Services.Foundations.Applicants
 {
-    public class ApplicantService
+    public class ApplicantService : IApplicantService
     {
+        private readonly IStorageBroker storageBroker;
+
+        public ApplicantService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<Applicant> AddApplicantAsync(Applicant applicant) =>
+            await this.storageBroker.InsertApplicantAsync(applicant);
+
+        public async ValueTask<Applicant> RetrieveApplicantByIdAsync(Guid applicantid) =>
+            await this.storageBroker.SelectApplicantByIdAsync(applicantid);
+
+        public IQueryable RetrieveAllApplicants() =>
+            this.storageBroker.SelectAllApplicants();
+
+        public async ValueTask<Applicant> ModifyApplicantAsync(Applicant applicant) =>
+            await this.storageBroker.UpdateAppolicantAsync(applicant);
+
+        public async ValueTask<Applicant> RemoveApplicantAsync(Guid guid)
+        {
+            Applicant applicant = await this.storageBroker.SelectApplicantByIdAsync(guid);
+
+            return await this.storageBroker.DeleteApplicantAsync(applicant);
+        }
     }
 }
---
+++ SmartManager/Services/Foundations/Applicants/IApplicantService.cs
@@ -0,0 +1,21 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using SmartManager.Models.Applicants;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartManager.Services.Foundations.Applicants
+{
+    public interface IApplicantService
+    {
+        ValueTask<Applicant> AddApplicantAsync(Applicant applicant);
+        ValueTask<Applicant> RetrieveApplicantByIdAsync(Guid applicantid);
+        IQueryable RetrieveAllApplicants();
+        ValueTask<Applicant> ModifyApplicantAsync(Applicant applicant);
+        ValueTask<Applicant> RemoveApplicantAsync(Guid guid);
+    }
+}
