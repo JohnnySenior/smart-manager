57ad56b58f327d7f39baf3a4a8f6b05f35beb78e
Zafar <icom@MACKBOOK>
2023-10-28 21:23:25 +0300

    BROKERS: Create Logging Broker

---
+++ SmartManager/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using System;
+
+namespace SmartManager.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        public void LogCritical(Exception exception);
+        public void LogError(Exception exception);
+    }
+}
