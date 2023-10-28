53efb450612746a10f2905208d90d82a83530628
Zafar <icom@MACKBOOK>
2023-10-28 21:52:44 +0300

    BROKERS: CRUD Group

--- SmartManager/Brokers/Storages/IStorageBroker.Groups.cs
+++ SmartManager/Brokers/Storages/IStorageBroker.Groups.cs
@@ -3,9 +3,19 @@
 // Managre quickly and easy
 //===========================
 
+using SmartManager.Models.Groups;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace SmartManager.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Group> InsertGroupAsync(Group group);
+        IQueryable<Group> SelectAllGroups();
+        ValueTask<Group> SelectGroupByIdAsync(Guid groupId);
+        ValueTask<Group> UpdateAppolicantAsync(Group group);
+        ValueTask<Group> DeleteGroupAsync(Group group);
     }
 }
--- SmartManager/Brokers/Storages/StorageBroker.Groups.cs
+++ SmartManager/Brokers/Storages/StorageBroker.Groups.cs
@@ -4,12 +4,31 @@
 //===========================
 
 using Microsoft.EntityFrameworkCore;
+using SmartManager.Models.Applicants;
 using SmartManager.Models.Groups;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
 
 namespace SmartManager.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Group> Groups { get; set; }
+
+        public async ValueTask<Group> InsertGroupAsync(Group group) =>
+           await InsertAsync(group);
+
+        public IQueryable<Group> SelectAllGroups() =>
+            SelectAll<Group>().AsQueryable();
+
+        public async ValueTask<Group> SelectGroupByIdAsync(Guid groupId) =>
+            await SelectAsync<Group>(groupId);
+
+        public async ValueTask<Group> UpdateAppolicantAsync(Group group) =>
+            await UpdateAsync(group);
+
+        public async ValueTask<Group> DeleteGroupAsync(Group group) =>
+            await DeleteAsync(group);
     }
 }
