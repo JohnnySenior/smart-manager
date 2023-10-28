bb835715134cf6c9493d497023588019d507cdcb
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-28 23:30:34 +0900

    ORCHESTRATIONS: Get Valid Applicants

---
+++ SmartManager/Services/Orchestrations/IOrchestrationService.cs
@@ -0,0 +1,15 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SmartManager.Services.Orchestrations
+{
+    public interface IOrchestrationService
+    {
+        Task ProcessImportRequest(MemoryStream stream);
+    }
+}
---
+++ SmartManager/Services/Orchestrations/OrchestrationService.cs
@@ -0,0 +1,29 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using SmartManager.Models.ExternalApplicants;
+using SmartManager.Services.Proccessings;
+
+namespace SmartManager.Services.Orchestrations
+{
+    public class OrchestrationService : IOrchestrationService
+    {
+        private readonly ISpreadsheetsProcessingService spreadsheetProcessingService;
+
+        public OrchestrationService(ISpreadsheetsProcessingService spreadsheetProcessingService)
+        {
+            this.spreadsheetProcessingService = spreadsheetProcessingService;
+        }
+
+        public async Task ProcessImportRequest(MemoryStream stream)
+        {
+            List<ExternalApplicant> validExternalApplicants =
+                this.spreadsheetProcessingService.ReadExternalApplicants(stream);
+        }
+    }
+}
