b4703af8033a79eb0d4f8b395ccbd0a2cbb710b2
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-28 23:10:12 +0900

    PROCCESSINGS: Read External Applicants

---
+++ SmartManager/Services/Proccessings/SpreadsheetsProcessingService.cs
@@ -0,0 +1,30 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Collections.Generic;
+using System.IO;
+using SmartManager.Models.ExternalApplicants;
+using SmartManager.Services.Foundations.Spreadsheets;
+
+namespace SmartManager.Services.Proccessings
+{
+    public class SpreadsheetsProcessingService : ISpreadsheetsProcessingService
+    {
+        private readonly ISpreadsheetService spreadsheetService;
+
+        public SpreadsheetsProcessingService(ISpreadsheetService spreadsheetService)
+        {
+            this.spreadsheetService = spreadsheetService;
+        }
+
+        public List<ExternalApplicant> ReadExternalApplicants(MemoryStream stream)
+        {
+            List<ExternalApplicant> validExternalApplicants =
+                this.spreadsheetService.GetExternalApplicants(stream);
+
+            return validExternalApplicants;
+        }
+    }
+}
